86a5201373caad7a85e594f735dceb9480943fff
Elvissamir <elvissamir18@gmail.com>
2022-05-11 06:31:52 -0400

    Add checkbox input for roles in EditUserForm component

--- src/components/EditUserForm.jsx
+++ src/components/EditUserForm.jsx
@@ -1,24 +1,31 @@
 import Joi from 'joi'
-import useForm from './hooks/useForm'
+import { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import useForm from './hooks/useForm'
+import routes from '../routes';
 import Form from './common/Form';
 import InputField from './common/InputField';
 import FormFooter from './common/FormFooter';
 import FormButton from './common/FormButton';
 import { updateUser } from '../services/usersService';
-import routes from '../routes';
 
 function EditUserForm () {
     const navigate = useNavigate()
     const { state: { user } } = useLocation()
 
+    const [isChecked, setIsChecked] = useState(false)
+
     const dataInit = {
         user: user.user,
+        roles: user.roles? user.roles : [],
         password: user.password
     }
 
+    const roleOption = 'admin'
+
     const dataSchema = {
         user: Joi.string().required().label('User'),
+        roles: Joi.array().required(),
         password: Joi.string().required().label('Password')
     }
 
@@ -26,6 +33,7 @@ function EditUserForm () {
         formData, 
         formErrors, 
         setFormErrors,
+        setFormData,
         validate, 
         handleChange
     } = useForm(dataInit, dataSchema)   
@@ -34,6 +42,7 @@ function EditUserForm () {
         e.preventDefault()
 
         try {
+            console.log(formData)
             await updateUser(dataInit.user, formData)
             navigate(routes.admin.users.url, { replace: true })
         } 
@@ -47,6 +56,13 @@ function EditUserForm () {
         }
     }
 
+    const handleOnChange = () => {
+        setIsChecked(!isChecked)
+        let temp = {...formData}
+        !isChecked? temp.roles.push(roleOption) : temp.roles.pop()
+        setFormData(temp)
+     };
+
     return (
         <div className='content-wrapper'>
             <Form size='w-6/12' title='Edit User' handleSubmit={ handleSubmit }>
@@ -57,6 +73,20 @@ function EditUserForm () {
                     value={ formData.user }
                     error={ formErrors.user }
                     handleChange={ handleChange } />
+
+                <div className='flex-row mt-5'>
+                    <label htmlFor="role">Admin</label>
+                    <input
+                            type="checkbox"
+                            className='ml-2'
+                            id="roles"
+                            name="role"
+                            value="roles"
+                            checked={isChecked}
+                            onChange={handleOnChange}
+                            />
+                </div>
+
                 <InputField 
                     label='Password'
                     id='password'
